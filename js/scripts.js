53a3da07159969c98f0cce6d9fb176cdedc63477
Student <example@example.com>
2019-10-28 13:55:15 -0700

    add looping and adjust HTML to fully match the story's missing information

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
+    event.preventDefault();
 
-    var inputperson = $('input#person').val();
-    var inputanimal = $('input#animal').val();
-    var inputnoun = $('input#noun').val();
-    var inputverb = $('input#verb').val();
-    var inputadjective = $('input#adjective').val();
+    // var inputperson = $('input#person').val();
+    // var inputanimal = $('input#animal').val();
+    // var inputnoun = $('input#noun').val();
+    // var inputverb = $('input#verb').val();
+    // var inputadjective = $('input#adjective').val();
+    //
+    // $(".person1").text(inputperson);
 
-    $(".person1").text(inputperson);
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"]
+
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#Story").show();
-    event.preventDefault();
-  })
-})
+  });
+});
