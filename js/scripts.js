a32870d5730cc8411593be650f9bb7464dc7c4be
Student <example@example.com>
2019-10-29 10:31:38 -0700

    change forEach loop to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,17 @@ $(document).ready(function() {
     // $(".person1").text(inputperson);
 
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"]
+    //
+    // blanks.forEach(function(blank){
+      // var userInput = $("input#" + blank).val();
+      // $("." + blank).text(userInput);
+    // });
 
-    blanks.forEach(function(blank){
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
+    for (var i = 0; i<=blanks.length; i += 1) {
+      var userInput = $("input#" + blanks[i]).val();
+      $("." + blanks[i]).text(userInput);
+
+    }
 
     $("#Story").show();
   });
